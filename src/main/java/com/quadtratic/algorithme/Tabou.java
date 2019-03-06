e671427ec46d69363d639d4e3dac884e4dfd8e8b
Stefano BEZABEH MEUCCI <stefanomeucci@hotmail.com>
2019-03-06 09:11:50 +0100

    Tabou class inialisation

---
+++ src/main/java/com/quadtratic/algorithme/Tabou.java
@@ -0,0 +1,6 @@
+package com.quadtratic.algorithme;
+
+public class Tabou {
+
+    public double tabuSearch (){return 0.0;};
+}
--- src/main/java/com/quadtratic/algorithme/main.java
+++ src/main/java/com/quadtratic/algorithme/main.java
@@ -1,4 +1,4 @@
-package com.quadtratic.models;
+package com.quadtratic.algorithme;
 
 public class main {
     public static void main (String[] args){
