7e5bc04c4a9e6bc774faae9e3243f4d2de1cfb72
CyprienF <floquet.cyprien@gmail.com>
2019-05-12 20:30:51 +0200

    Refactorisation TatouTest

--- src/test/java/com/quadtratic/algorithme/TabouTest.java
+++ src/test/java/com/quadtratic/algorithme/TabouTest.java
@@ -1,47 +1,62 @@
 package com.quadtratic.algorithme;
 
-import org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class TabouTest {
 
-    @org.junit.jupiter.api.Test
-    void tabuSearch() {
-        int[] i = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    private Quadratic quadratic;
+    private int[] solutions;
 
-        //test Tabou 193 Iteration
-        Quadratic q1 = new Quadratic("tai12a.dat");
+    @BeforeEach
+    void setUp() {
+        quadratic = new Quadratic("tai12a.dat");
+        solutions = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    }
+
+    @Test
+    void tabuSearch193Iterations() {
+
         Solution s1 = new Solution();
-        s1.setSolution(i);
-        Tabou t = new Tabou(q1, 193, 9, true);
+        s1.setSolution(solutions);
+
+        Tabou t = new Tabou(quadratic, 193, 9, true);
         Solution optimale1 = t.tabuSearch(s1);
-        int[] expectedSolution1 = {8,1,6,2,11,10,3,5,9,7,12,4};
-        assertEquals(112208.0, optimale1.getFitness(), "For 193 Iterations");
-        assertArrayEquals(expectedSolution1, optimale1.getSolution());
 
+        int[] expectedSolution1 = {8, 1, 6, 2, 11, 10, 3, 5, 9, 7, 12, 4};
 
-        //test Tabou 6 Iteration
-        Quadratic q2 = new Quadratic("tai12a.dat");
+        Assertions.assertEquals(112208.0, optimale1.getFitness());
+        Assertions.assertArrayEquals(expectedSolution1, optimale1.getSolution());
+    }
+
+    @Test
+    void tabuSearch7Iterations() {
+
         Solution s2 = new Solution();
-        s2.setSolution(i);
-        Tabou t2 = new Tabou(q2, 7, 9, true);
+        s2.setSolution(solutions);
+
+        Tabou t2 = new Tabou(quadratic, 7, 9, true);
+
         Solution optimale2 = t2.tabuSearch(s2);
-        int[] expectedSolution2 = {10,12,2,6,4,5,7,9,8,1,11,3};
-        assertEquals(129268.0, optimale2.getFitness(), "For 7 Iterations");
-        assertArrayEquals(expectedSolution2, optimale2.getSolution());
+        int[] expectedSolution2 = {10, 12, 2, 6, 4, 5, 7, 9, 8, 1, 11, 3};
 
-        //test Tabou 2000 Iteration
-        Quadratic q3 = new Quadratic("tai12a.dat");
+        Assertions.assertEquals(129268.0, optimale2.getFitness());
+        Assertions.assertArrayEquals(expectedSolution2, optimale2.getSolution());
+    }
+
+    @Test
+    void tabuSearch2000Iterations() {
+
         Solution s3 = new Solution();
-        s3.setSolution(i);
-        Tabou t3 = new Tabou(q3, 2000, 9, true);
+        s3.setSolution(solutions);
+        
+        Tabou t3 = new Tabou(quadratic, 2000, 9, true);
+
         Solution optimale3 = t3.tabuSearch(s3);
-        int[] expectedSolution3 = {8,1,6,2,11,10,3,5,9,7,12,4};
+        int[] expectedSolution3 = {8, 1, 6, 2, 11, 10, 3, 5, 9, 7, 12, 4};
 
-        assertEquals(112208.0, optimale3.getFitness(), "For 193 Iterations");
-        assertArrayEquals(expectedSolution3, optimale3.getSolution());
-
+        Assertions.assertEquals(112208.0, optimale3.getFitness());
+        Assertions.assertArrayEquals(expectedSolution3, optimale3.getSolution());
     }
 }
