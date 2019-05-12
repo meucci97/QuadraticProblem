5f16f1aa687768438057b8491e8a0968867c1d0e
CyprienF <floquet.cyprien@gmail.com>
2019-05-12 20:34:40 +0200

    Remove unused imports

--- src/main/java/com/quadtratic/algorithme/Solution.java
+++ src/main/java/com/quadtratic/algorithme/Solution.java
@@ -1,8 +1,5 @@
 package com.quadtratic.algorithme;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
 
     private double fitness;
--- src/main/java/com/quadtratic/algorithme/main.java
+++ src/main/java/com/quadtratic/algorithme/main.java
@@ -1,7 +1,5 @@
 package com.quadtratic.algorithme;
 
-import java.util.ArrayList;
-
 public class main {
     public static void main (String[] args){
         /*
